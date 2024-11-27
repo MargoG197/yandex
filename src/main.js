5db9f63314f45a0376d21f5013e4f768f13ac376
Goncharova Margo <goncharovarita@mail.ru>
2024-11-27 22:11:52 +0300

    added second div

--- src/main.js
+++ src/main.js
@@ -2,23 +2,8 @@ import './style.css'
 import logo from './img/logo.svg'
 import round from './img/Ellipse.svg'
 import group from './img/Group.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
-// <a href="https://vite.dev" target="_blank">
-    //   <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    // </a>
-    // <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-    //   <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    // </a>
-    // <h1>Hello Vite!</h1>
-    // <div class="card">
-    //   <button id="counter" type="button"></button>
-    // </div>
-    // <p class="read-the-docs">
-    //   Click on the Vite logo to learn more
-    // </p>
-
-document.querySelector('#app').innerHTML = `
+import pic from './img/Frame 48095654.png'
+document.querySelector('#div1').innerHTML = `
   <div class="top_div">
         <img src="${logo}" class="logo" alt="logo" />
         <div class="top_div_content">
@@ -40,7 +25,56 @@ document.querySelector('#app').innerHTML = `
           </div>
         </div>
       </div>
-			<div class="string">Лёд тронулся, господа присяжные заседатели! Шахматы двигают вперёд не только культуру, но и экономику! Дело помощи утопающим — дело рук самих утопающих!</div>
+
+			<div>
+			<section class="string" >
+			<div class="div1" >
+			 Лёд тронулся, господа присяжные заседатели! • Шахматы двигают вперёд не только культуру, но и экономику! • Дело помощи утопающим — дело рук самих утопающих! • 
+			</div>
+			<div  class="div2">
+			 Лёд тронулся, господа присяжные заседатели! • Шахматы двигают вперёд не только культуру, но и экономику! • Дело помощи утопающим — дело рук самих утопающих! • 
+			</div>
+			</section>
+			</div>
 `
 
-setupCounter(document.querySelector('#counter'))
+document.querySelector('#div2').innerHTML = `
+  <div class="div2_top_div">
+	<div class="div2_top_div_text">
+	Чтобы поддержать <br/>Международный васюкинский турнир посетите лекцию на тему: <b>«Плодотворная дебютная идея»</b>
+	</div>
+	<div class="div2_second_div">
+	<img src="${pic}"/>
+	<div class="div2_second_div_right_side">
+	<div class="div2_second_div_right_side_1">
+	и Сеанс <b>одновременной игры<br/> в шахматы на 160 досках</b> гроссмейстера О. Бендера
+	</div>
+	<div>
+	<table id="table" class="table">
+	<tr>
+	 <td>Место проведения:</td>
+	 <td>Клуб «Картонажник»</td>
+	</tr>
+	<tr>
+		<td>Дата и время мероприятия:</td>
+		<td>22 июня 1927 г. в 18:00</td>
+	</tr>
+	<tr>
+		<td>Стоимость входных билетов:</td>
+		<td>20 коп.</td>
+	</tr>
+		<tr>
+		<td>Плата за игру:</td>
+		<td>50 коп.</td>
+	</tr>
+		<tr>
+		<td>Взнос на телеграммы:</td>
+		<td><b>100 руб.</b> 21 руб. 16 коп.</td>
+	</tr>
+</table>
+	</div>
+	</div>
+	</div>
+        
+			</div>
+`
